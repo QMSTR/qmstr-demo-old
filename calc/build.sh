b78079ad926af9f75ff3c018ff106e81250b2d5d
Markus Herpich <markus@endocode.com>
2018-02-08 10:00:45 +0100

    Extract common functions
    Extract helper functions to be used from other demo build scripts.

--- calc/build.sh
+++ calc/build.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 set -e
 
+source ../build.inc
+init
+run_qmstr_master
+
 JSONC_BRANCH="master"
 
-function run_qmstr_prod() {
-    docker run --rm -d -p 50051:50051 -v $(pwd):/buildroot qmstr/master
-}
+setup_git_src https://github.com/json-c/json-c.git master json-c
 
-function run_qmstr_dev() {
-    if [ -z "$QMSTR_SRC" ]; then
-        echo "Please set QMSTR_SRC to point to your qmstr source directory."
-        exit 2
-    fi
-    docker run --rm -d -p 50051:50051 -p 8081:8081 -p 8080:8080 -v "${QMSTR_SRC}":/go/src/github.com/QMSTR/qmstr -v $(pwd):/buildroot qmstr/dev
-
-}
-
-if [ -z "$QMSTR_HOME" ]; then
-    echo "Please set QMSTR_HOME. See github.com/QMSTR/qmstr-demo."
-    exit 1
-fi
-
-export PATH=$QMSTR_HOME/bin:$PATH
-
-if [ -z "$QMSTR_DEBUG" ]; then
-    run_qmstr_prod
-else
-    run_qmstr_dev
-fi
-
-git clone -b ${JSONC_BRANCH} https://github.com/json-c/json-c.git || (cd json-c; git fetch; git reset --hard origin/${JSONC_BRANCH})
 cd json-c
 git clean -fxd
 
