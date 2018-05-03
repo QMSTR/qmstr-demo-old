e6caeb36b9b938a4e46386b6381f314808905677
Markus Herpich <markus@endocode.com>
2018-05-03 13:08:21 +0200

    Parametrize demo run script
    Allow choosing the demo to run via command line.

--- rundemo.sh
+++ rundemo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DEMO=curl
+DEMO=${1:-curl}
 HOSTDEMODIR=$(pwd)/demos
 
 docker run --name demo --privileged -v /var/run/docker.sock:/var/run/docker.sock -v $GOPATH/src:/go/src -v $HOSTDEMODIR:/demos -e HOSTDEMODIR=$HOSTDEMODIR/$DEMO --net qmstrnet --rm qmstr/demo$DEMO
