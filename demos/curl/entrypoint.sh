80a3973648cf5401f68c7ee44fa4fd946896d909
Yotam <yotam.cohen@endocode.com>
2018-05-07 16:10:37 +0200

    Change path of report file
    match new output path inside master container

--- demos/curl/entrypoint.sh
+++ demos/curl/entrypoint.sh
@@ -53,7 +53,7 @@ qmstr-cli $ADDRESS analyze
 echo "[INFO] Analysis finished. Triggering reporting."
 qmstr-cli $ADDRESS report
 
-docker cp ${MASTER_CONTAINER_NAME}:/qmstr-reports.tar.bz2 ${DEMOWD}
+docker cp ${MASTER_CONTAINER_NAME}:/var/qmstr/qmstr-reporter-html/qmstr-reports.tar.bz2 ${DEMOWD}
 
 # Remove curl folder
 echo "deleting temporary directory curl"
