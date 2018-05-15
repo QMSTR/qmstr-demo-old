ece99d886986778a3cbdcf548ad1570a9fd1c7c8
Markus Herpich <markus@endocode.com>
2018-05-15 15:47:23 +0200

    Remove obsolete run script
    Use make

--- rundemo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-DEMO=${1:-curl}
-HOSTDEMODIR=$(pwd)/demos
-echo "docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock -v $GOPATH/src:/go/src -v $HOSTDEMODIR:/demos -e MASTER_CONTAINER_NAME=${MASTER_CONTAINER_NAME} -e HOSTDEMODIR=$HOSTDEMODIR/$DEMO --net qmstrnet --rm qmstr/demo$DEMO"
-docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock -v $GOPATH/src:/go/src -v $HOSTDEMODIR:/demos -e MASTER_CONTAINER_NAME=${MASTER_CONTAINER_NAME} -e HOSTDEMODIR=$HOSTDEMODIR/$DEMO --net qmstrnet --rm qmstr/demo$DEMO
