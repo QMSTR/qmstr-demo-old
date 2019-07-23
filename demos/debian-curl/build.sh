45baa5212a8ec421149c4a2d9e804d3d1767df2e
GiasemiSh <giasemi.seisa@endocode.com>
2019-07-23 15:00:14 +0200

    [debian curl] Add the rest of the debian pkgs
    Include the rest of the debian packages and their targets

--- demos/debian-curl/build.sh
+++ demos/debian-curl/build.sh
@@ -22,12 +22,20 @@ echo "master server up and running"
 
 echo "[INFO] Create packages"
 qmstrctl create package:curl_7.64.0-3_amd64.deb --version $(cd curl && git describe --tags --dirty --long)
+qmstrctl create package:libcurl4_7.64.0-3_amd64.deb
+qmstrctl create package:libcurl3-gnutls_7.64.0-3_amd64.deb
+qmstrctl create package:libcurl3-nss_7.64.0-3_amd64.deb
+qmstrctl create package:libcurl4-openssl-dev_7.64.0-3_amd64.deb
+qmstrctl create package:libcurl4-gnutls-dev_7.64.0-3_amd64.deb
+qmstrctl create package:libcurl4-nss-dev_7.64.0-3_amd64.deb
 
 echo "[INFO] Start debian curl build"
 qmstrctl --verbose spawn qmstr/debian-curldemo qmstr run dpkg-buildpackage -B -us -uc
+qmstrctl snapshot -O priortargets-snapshot.tar -f
 
-echo "[INFO] Connect targets to packages"
+echo "[INFO] Connecting targets to packages"
 ./targets.sh
+echo "[INFO] Done"
 
 echo "[INFO] Build finished. Creating snapshot and triggering analysis."
 qmstrctl snapshot -O postbuild-snapshot.tar -f
